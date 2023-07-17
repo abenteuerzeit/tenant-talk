281b75a9aa5d511db8eb30889e77236a408a3379
Piotr Palacz <peterpalacz12@gmail.com>
2023-07-17 21:29:55 +0200

    style: read username and avatar from cookies, responsive page layout created

--- tenant_frontend/src/pages/UserProfile.jsx
+++ tenant_frontend/src/pages/UserProfile.jsx
@@ -1,38 +1,108 @@
 import React from "react";
-// import Cookies from "js-cookie";
-import { useUser } from "../components/auth_components/userProvider"
-import { Avatar, Box, Center, Heading, Text } from "@chakra-ui/react";
-
+import { useUser } from "../components/auth_components/userProvider";
+import {
+    Avatar,
+    Box,
+    Center,
+    Heading,
+    Text,
+    Stack,
+    Badge,
+    AvatarBadge,
+} from "@chakra-ui/react";
+import { EditIcon } from "@chakra-ui/icons";
+import { Link } from "react-router-dom";
 
 export const UserProfile = () => {
-  const userData = useUser();
-  
-  return (
-    <>
-    <Center>
-      <Box display={"flex"} flexDirection={"column"} alignItems={"center"}>
-        <Box>
-          <Avatar border={"1px solid red"} m={4} size="2xl" src={userData.user.profile.picture} />
-        </Box>
+    const userData = useUser();
+    console.log(userData);
 
-        <Box>
-          <Heading>
-            Hello, {userData.user.profile.given_name} !
-          </Heading>
-        </Box>
+    return (
+        <>
+            <Center bg={"rgba(207, 213, 222)"} p={2}>
+                <Stack
+                    w={"96%"}
+                    direction={["column", "column", "row"]}
+                    spacing={4}
+                >
+                    <Box
+                        p={5}
+                        w={["100%", "100%", "30%"]}
+                        bg="white"
+                        h={"100%"}
+                        borderRadius={"2px"}
+                        textAlign={"center"}
+                    >
+                        <Heading as={"h1"}>
+                            {userData.user.profile.name}
+                        </Heading>
+                        <Text>
+                            @
+                            {userData.user.profile.name
+                                .replace(/\s/g, "")
+                                .toLowerCase()}
+                        </Text>
+                        <Avatar
+                            m={4}
+                            size="2xl"
+                            src={userData.user.profile.picture}
+                        >
+                            <AvatarBadge boxSize={"70px"} border={"none"}>
+                                <Avatar
+                                    icon={<EditIcon />}
+                                    bg={"red"}
+                                    size={"sm"}
+                                />
+                            </AvatarBadge>
+                        </Avatar>
+                    </Box>
 
-        <Box>
-          <Text>
-            This is your profile page. You can view your profile information and change your details if you feel like.
-          </Text>
-        </Box>
-
-      </Box>
-      
-    </Center>
-    
-    
-      
-    </>
-  );
+                    <Box
+                        p={5}
+                        w={["100%", "100%", "70%"]}
+                        bg="white"
+                        borderRadius={"2px"}
+                    >
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Consequatur, reiciendis deserunt? Deserunt quia deleniti
+                        placeat nulla quasi doloribus, optio unde illo dolores
+                        neque voluptates, laborum, pariatur voluptas hic totam
+                        assumenda.
+                        <br />
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Consequatur, reiciendis deserunt? Deserunt quia deleniti
+                        placeat nulla quasi doloribus, optio unde illo dolores
+                        neque voluptates, laborum, pariatur voluptas hic totam
+                        assumenda.
+                        <br />
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Consequatur, reiciendis deserunt? Deserunt quia deleniti
+                        placeat nulla quasi doloribus, optio unde illo dolores
+                        neque voluptates, laborum, pariatur voluptas hic totam
+                        assumenda.
+                        <br />
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Consequatur, reiciendis deserunt? Deserunt quia deleniti
+                        placeat nulla quasi doloribus, optio unde illo dolores
+                        neque voluptates, laborum, pariatur voluptas hic totam
+                        assumenda.
+                        <br />
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Consequatur, reiciendis deserunt? Deserunt quia deleniti
+                        placeat nulla quasi doloribus, optio unde illo dolores
+                        neque voluptates, laborum, pariatur voluptas hic totam
+                        assumenda.
+                        <br />
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Consequatur, reiciendis deserunt? Deserunt quia deleniti
+                        placeat nulla quasi doloribus, optio unde illo dolores
+                        neque voluptates, laborum, pariatur voluptas hic totam
+                        assumenda.
+                        <br />
+                    </Box>
+                </Stack>
+            </Center>
+        </>
+    );
 };
+
