c2cd29b40efdb3daae75b97e9d0926bb296270fe
abenteuerzeit <dr.adrian.mroz@outlook.com>
2023-07-19 15:50:30 +0200

    fix(routing): use HashRouter to enable correct routing on GitHub Pages

--- tenant_frontend/src/index.js
+++ tenant_frontend/src/index.js
@@ -12,7 +12,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
         <I18Provider i18n={i18n}>
-            <Router basename="/tenant-talk">
+            <Router>
                 <App />
             </Router>
         </I18Provider>
