916d6e8bad7ab91ce41c5e5a20695c907f68baba
Adrian Mróz, Ph.D <dr.adrian.mroz@outlook.com>
2023-07-19 14:24:04 +0200

    feat: update router base path for GitHub Pages deployment

--- tenant_frontend/src/index.js
+++ tenant_frontend/src/index.js
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
         <I18Provider i18n={i18n}>
-            <Router>
+            <Router basename="/tenant-talk">
                 <App />
             </Router>
         </I18Provider>
